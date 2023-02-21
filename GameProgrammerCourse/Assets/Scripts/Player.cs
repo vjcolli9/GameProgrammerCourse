ff7ffb6f9b82ff9ce18e38374f690c94dc0ad470
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-02-21 12:11:28 -0800

    [SerializedField]

--- GameProgrammerCourse/Assets/Scripts/Player.cs
+++ GameProgrammerCourse/Assets/Scripts/Player.cs
@@ -1,19 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]float _speed = 1;
 
     // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal") * _speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
     }
