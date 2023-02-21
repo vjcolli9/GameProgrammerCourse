c5d19bddc30c98065b614e615f9990e894eeb75b
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-02-21 13:38:15 -0800

    Animator

--- GameProgrammerCourse/Assets/Scripts/Player.cs
+++ GameProgrammerCourse/Assets/Scripts/Player.cs
@@ -10,5 +10,10 @@ public class Player : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal") * _speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+        Debug.Log($"Velocity = {rigidbody2D.velocity}");
+
+        var animator = GetComponent<Animator>();
+        bool walking = horizontal != 0;
+        animator.SetBool("Walk", walking);
     }
 }
