e225d13018de4758347936aff7633e5cabe18c14
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-02-25 11:03:15 -0800

    Fly move up & down

---
+++ GameProgrammerCourse/Assets/Scripts/Fly.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    Vector2 _startPosition;
+    Vector2 _direction = Vector2.up;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_direction * Time.deltaTime);
+        var distance = Vector2.Distance(_startPosition, transform.position);
+        if(distance >= 2)
+        {
+            _direction *= -1;
+        }
+    }
+}
