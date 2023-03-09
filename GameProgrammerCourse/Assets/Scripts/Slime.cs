ab49252aa8f78e3dcccc7a399e3e3ee0ed1bb426
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-03-08 23:10:28 -0800

    Add Sensors to slime

--- GameProgrammerCourse/Assets/Scripts/Slime.cs
+++ GameProgrammerCourse/Assets/Scripts/Slime.cs
@@ -1,21 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] Transform _leftSensor;
+    [SerializeField] Transform _rightSensor;
     Rigidbody2D _rigidbody2D;
+    float _direction = -1f;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rigidbody2D.velocity = new Vector2(-1f, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = new Vector2(_direction, _rigidbody2D.velocity.y);
+        if (_direction < 0)
+        {
+            Debug.DrawRay(_leftSensor.position, Vector2.down * 0.1f, Color.red);
+
+            RaycastHit2D result = Physics2D.Raycast(_leftSensor.position, Vector2.down, 0.1f);
+            RaycastHit2D wallResult = Physics2D.Raycast(_leftSensor.position, Vector2.left, 0.1f);
+
+            if (result.collider == null)
+                TurnAround();
+
+            if (wallResult.collider != null)
+                TurnAround();
+        }
+        else
+        {
+            Debug.DrawRay(_rightSensor.position, Vector2.down * 0.1f, Color.red);
+
+            var result = Physics2D.Raycast(_rightSensor.position, Vector2.down, 0.1f);
+            RaycastHit2D wallResult = Physics2D.Raycast(_rightSensor.position, Vector2.right, 0.1f);
+
+            if (result.collider == null)
+                TurnAround();
+
+            if (wallResult.collider != null)
+                TurnAround();
+        }
+        
+    }
+
+    void TurnAround()
+    {
+        _direction *= -1;
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = _direction > 0;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
