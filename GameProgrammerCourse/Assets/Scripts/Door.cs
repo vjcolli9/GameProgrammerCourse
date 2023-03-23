66107f4678bc0213dcf4e441e40540b5bfbb12b9
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-03-23 14:33:36 -0700

    Add Working Door UI

--- GameProgrammerCourse/Assets/Scripts/Door.cs
+++ GameProgrammerCourse/Assets/Scripts/Door.cs
@@ -11,18 +11,24 @@ public class Door : MonoBehaviour
     [SerializeField] SpriteRenderer _rendererTop;
     [SerializeField] int _requiredCoins = 3;
     [SerializeField] Door _exit;
+    [SerializeField] Canvas _canvas;
+
+    bool _open;
 
     //[ContextMenu("Open Door")]
     void Open()
     {
+        _open = true;
         _rendererMid.sprite = _openMid;
         _rendererTop.sprite = _openTop;
+        if(_canvas != null)
+            _canvas.enabled = false;
     }
 
     
     void Update()
     {
-        if(Coin.CoinsCollected >= _requiredCoins)
+        if(_open == false && Coin.CoinsCollected >= _requiredCoins)
         {
             Open();
         }
@@ -30,6 +36,9 @@ public class Door : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (_open == false)
+            return;
+        
         var player = collision.GetComponent<Player>();
         if (player != null && _exit != null)
         {
