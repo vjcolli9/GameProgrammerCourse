5b72fb3e0ef98f573fc6b807c5503cea9a0c31ba
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-03-23 12:07:55 -0700

    Added a Door that opens

---
+++ GameProgrammerCourse/Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Sprite _openMid;
+    [SerializeField] Sprite _openTop;
+
+    [SerializeField] SpriteRenderer _rendererMid;
+    [SerializeField] SpriteRenderer _rendererTop;
+
+    [ContextMenu("Open Door")]
+    void Open()
+    {
+        _rendererMid.sprite = _openMid;
+        _rendererTop.sprite = _openTop;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
