2c0dbcf37b098336351cc4b400d6272fd260f61f
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-03-23 11:47:47 -0700

    Added UI Coin Counter

--- GameProgrammerCourse/Assets/Scripts/Coin.cs
+++ GameProgrammerCourse/Assets/Scripts/Coin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    static int _coinsCollected;
+    public static int CoinsCollected;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,7 +13,7 @@ public class Coin : MonoBehaviour
             return;
 
         gameObject.SetActive(false);
-        _coinsCollected++;
-        Debug.Log(_coinsCollected);
+        CoinsCollected++;
+        Debug.Log(CoinsCollected);
     }
 }
