11cd78fbe08572f0432dfc23094599bf432b4460
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-03-24 12:29:57 -0700

    Implemented Wiggle Functionality

--- GameProgrammerCourse/Assets/Scripts/FallingPlatform.cs
+++ GameProgrammerCourse/Assets/Scripts/FallingPlatform.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     public bool PlayerInside;
-
+    Coroutine _coroutine;
     HashSet<Player> _playersInTrigger = new HashSet<Player>();
+    Vector3 _initialPosition;
+
+    void Start()
+    {
+        _initialPosition = transform.position;
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,7 +23,8 @@ public class FallingPlatform : MonoBehaviour
         _playersInTrigger.Add(player);
         PlayerInside = true;
 
-        StartCoroutine(WiggleAndFall());
+        if(_playersInTrigger.Count == 1)
+            _coroutine = StartCoroutine(WiggleAndFall());
     }
 
     IEnumerator WiggleAndFall()
@@ -25,6 +32,16 @@ public class FallingPlatform : MonoBehaviour
         Debug.Log("Waiting to wiggle");
         yield return new WaitForSeconds(0.25f);
         Debug.Log("Wiggling");
+        float wiggleTimer = 0f;
+        while (wiggleTimer < 1f)
+        {
+            float randomX = UnityEngine.Random.Range(-0.05f, 0.01f);
+            float randomY = UnityEngine.Random.Range(-0.05f, 0.01f);
+            transform.position = _initialPosition + new Vector3(randomX, randomY);
+            float randomDelay = UnityEngine.Random.Range(0.005f, 0.01f);
+            yield return new WaitForSeconds(randomDelay);
+            wiggleTimer += randomDelay;
+        }
         yield return new WaitForSeconds(1f);
         Debug.Log("Falling");
         yield return new WaitForSeconds(3f);
@@ -38,6 +55,9 @@ public class FallingPlatform : MonoBehaviour
         _playersInTrigger.Remove(player);
         
         if(_playersInTrigger.Count == 0)
+        {
             PlayerInside = false;
+            StopCoroutine(_coroutine);
+        }
     }
 }
