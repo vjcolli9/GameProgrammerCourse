751e5191c20a559ffd2122bdc48aa02a97ca0e5b
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-03-23 18:58:24 -0700

    Limit Player per Plat + Wiggle Method

--- GameProgrammerCourse/Assets/Scripts/FallingPlatform.cs
+++ GameProgrammerCourse/Assets/Scripts/FallingPlatform.cs
@@ -6,19 +6,38 @@ public class FallingPlatform : MonoBehaviour
 {
     public bool PlayerInside;
 
+    HashSet<Player> _playersInTrigger = new HashSet<Player>();
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
         if (player == null)
             return;
+
+        _playersInTrigger.Add(player);
         PlayerInside = true;
+
+        StartCoroutine(WiggleAndFall());
     }
 
+    IEnumerator WiggleAndFall()
+    {
+        Debug.Log("Waiting to wiggle");
+        yield return new WaitForSeconds(0.25f);
+        Debug.Log("Wiggling");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Falling");
+        yield return new WaitForSeconds(3f);
+    }
     void OnTriggerExit2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
         if (player == null)
             return;
-        PlayerInside = false;
+
+        _playersInTrigger.Remove(player);
+        
+        if(_playersInTrigger.Count == 0)
+            PlayerInside = false;
     }
 }
