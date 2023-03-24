1094ff0533009e542a9f9589a26cc8d168fdb84e
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-03-23 18:31:23 -0700

    Created PlayerInside Script

---
+++ GameProgrammerCourse/Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public bool PlayerInside;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+            return;
+        PlayerInside = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+            return;
+        PlayerInside = false;
+    }
+}
