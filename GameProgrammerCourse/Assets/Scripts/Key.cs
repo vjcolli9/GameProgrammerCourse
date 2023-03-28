11d4e5aea4e00f26eccf4bf2b582dd99144b4863
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-03-28 14:37:37 -0700

    Added Grabbable Key

---
+++ GameProgrammerCourse/Assets/Scripts/Key.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            transform.SetParent(player.transform);
+            transform.localPosition = Vector3.up;
+        }
+
+    }
+}
