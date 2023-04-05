aa5324c8871fb7b240c512e3bd022756dbdacda4
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-04-05 12:57:48 -0700

    Added UI Elements

--- GameProgrammerCourse/Assets/Scripts/Collector.cs
+++ GameProgrammerCourse/Assets/Scripts/Collector.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
     [SerializeField] Collectible[] _collectibles;
+    TMP_Text _remainingText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _remainingText = GetComponentInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        int countRemaining = 0;
         foreach(var collectible in _collectibles)
         {
             if (collectible.isActiveAndEnabled)
-                return;
+                countRemaining++;
         }
-        
+
+        //catches null exceptions with '?'
+        _remainingText?.SetText(countRemaining.ToString());
+
+        if (countRemaining > 0)
+            return;
 
         Debug.Log("Got All Gems");
     }
