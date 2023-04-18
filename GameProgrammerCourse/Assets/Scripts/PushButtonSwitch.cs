4fd414e85289b7e4290b1062cbd3c25518ec45b4
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-04-17 18:29:11 -0700

    OnReleased Functionality
    Checks if there is no _onReleased() option, if so, the button stays pressed down

--- GameProgrammerCourse/Assets/Scripts/PushButtonSwitch.cs
+++ GameProgrammerCourse/Assets/Scripts/PushButtonSwitch.cs
@@ -43,7 +43,10 @@ public class PushButtonSwitch : MonoBehaviour
 
     void BecomeReleased()
     {
-        _spriteRenderer.sprite = _releasedSprite;
-        _onReleased?.Invoke();
+        if(_onReleased.GetPersistentEventCount() != 0)
+        {
+            _spriteRenderer.sprite = _releasedSprite;
+            _onReleased?.Invoke();
+        }
     }
 }
