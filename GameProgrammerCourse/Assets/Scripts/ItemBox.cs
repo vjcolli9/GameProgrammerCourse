b8d4b55ab8aadb6ea409ffe0300f9a35b4a26844
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-04-19 14:12:38 -0700

    item box can spawn prefabs

--- GameProgrammerCourse/Assets/Scripts/ItemBox.cs
+++ GameProgrammerCourse/Assets/Scripts/ItemBox.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ItemBox : HittableFromBelow
 {
+    [SerializeField] GameObject _itemPrefab;
     [SerializeField] GameObject _item;
     [SerializeField] Vector2 _itemLaunchVelocity;
     bool _used;
@@ -12,9 +13,11 @@ public class ItemBox : HittableFromBelow
         _item.SetActive(false);
     }
 
-    protected override bool CanUse => _used == false && _item != null;
+    protected override bool CanUse => _used == false;
     protected override void Use()
     {
+        Debug.Log("Used Itembox");
+        _item = Instantiate(_itemPrefab, transform.position + Vector3.up, Quaternion.identity, transform);
         if (_item == null)
             return;
 
