52ccf864bfa712126b22f69e0a1409e294896179
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-05-11 17:39:04 -0700

    Updated Singleton pattern

--- GameProgrammerCourse/Assets/Scripts/Music.cs
+++ GameProgrammerCourse/Assets/Scripts/Music.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Music : MonoBehaviour
@@ -7,7 +5,15 @@ public class Music : MonoBehaviour
     public static Music Instance { get; private set; }
     void Awake()
     {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         
-        DontDestroyOnLoad(gameObject);
     }
 }
