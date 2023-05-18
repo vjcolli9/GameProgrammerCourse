6d48249a95c69fe3935e901003a786d74b28ad16
vjcolli9 <vjcolli9@csu.fullerton.edu>
2023-05-17 19:57:36 -0700

    Added Coin Variation

--- GameProgrammerCourse/Assets/Scripts/Coin.cs
+++ GameProgrammerCourse/Assets/Scripts/Coin.cs
@@ -6,13 +6,24 @@ public class Coin : MonoBehaviour
 {
     public static int CoinsCollected;
 
+    [SerializeField] List<AudioClip> _clips;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
         if (player == null)
             return;
-
-        GetComponent<AudioSource>().Play();
+        if(_clips.Count > 0)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, _clips.Count);
+            AudioClip clip = _clips[randomIndex];
+            GetComponent<AudioSource>().PlayOneShot(clip);
+        }  
+        else
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        
         GetComponent<Collider2D>().enabled = false;
         GetComponent<SpriteRenderer>().enabled = false;
         CoinsCollected++;
